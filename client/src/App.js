4c28d1ed8337be8a338a4e56ddb6e3b1e0d0a291
wafflemakr <wafflemakr@gmail.com>
2020-12-20 16:31:32 -0500

    upload button image

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,21 @@ import ipfs from "./utils/ipfs";
 import "./App.css";
 import image from "./assets/logo.png";
 import forgeButton from "./assets/forgeButton.png";
+import uploadButton from "./assets/uploadButton.png";
 
 function App() {
-  const [ipfsHash, setIpfsHash] = useState(null);
+  // File
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState(null);
 
-  console.log(file);
+  // Min Balance Condition
+  const [option1Checked, setOption1Checked] = useState(false);
+  const [tokenAddress, setTokenAddress] = useState(null);
+  const [minBalance, setMinBalance] = useState(0);
+
+  // Expiration Condition
+  const [option2Checked, setOption2Checked] = useState(false);
+  const [expirationTime, setExpirationTime] = useState(0);
 
   const loadFile = (_file) => {
     setFileName(_file.name);
@@ -25,13 +33,17 @@ function App() {
 
   const addToIpfs = async () => {
     console.log("adding to IPFS...");
-
     const added = await ipfs.add(file, {
       progress: (prog) => console.log(`received: ${prog}`),
     });
-    setIpfsHash(added.cid.toString());
+    return added.cid.toString();
+  };
 
-    console.log(ipfsHash);
+  const createToken = async () => {
+    console.log("Creating NFT...");
+
+    // const ipfsHash = await addToIpfs();
+    // console.log(ipfsHash);
   };
 
   return (
@@ -41,69 +53,136 @@ function App() {
           <Image src={image} className="logo"></Image>
         </Col>
         <Col>
-          {!file && (
-            <>
-              <div id="upload-container">
-                <div id="fileUpload">
-                  <input
-                    id="file"
-                    type="file"
-                    name="file"
-                    className="inputfile"
-                    onChange={(e) => loadFile(e.target.files[0])}
-                  />
-                  <label for="file" id="fileLabel">
-                    Upload
-                  </label>
-                </div>
-              </div>
-              <p>Please upload a PNG, GIF, WEBP, or MP4 Max 30mb</p>
-            </>
-          )}
-          {fileName && (
-            <label htmlFor="file" className="mb">
-              <strong>File Uploaded: </strong>
-              {fileName}
-            </label>
-          )}
-
-          <Form className="mt-5">
-            <Form.Group as={Row} controlId="formPlaintextPassword">
-              <Form.Label column sm="2">
-                NFT Name
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="text" placeholder="Eg. Blue ZUT Statue" />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} controlId="formPlaintextPassword">
-              <Form.Label column sm="2">
-                Symbol
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="text" placeholder="ZUT, ETH, ZRX ..." />
-              </Col>
-            </Form.Group>
-          </Form>
-          <p className="mt-5">
-            Please add specialized properties to the NFT (select one or
-            multiple):{" "}
-          </p>
+          {/* File Upload */}
           <div>
-            <Form.Check className="ml-3" type={"checkbox"}>
-              <Form.Check.Input type={"checkbox"} />
+            {!file && (
+              <>
+                <div id="upload-container">
+                  <div id="fileUpload">
+                    <input
+                      id="file"
+                      type="file"
+                      name="file"
+                      className="inputfile"
+                      onChange={(e) => loadFile(e.target.files[0])}
+                    />
+                    <label htmlFor="file" id="fileLabel">
+                      <img src={uploadButton} className="upload-button" />
+                    </label>
+                  </div>
+                </div>
+                <p className="mt-4">
+                  Please upload a PNG, GIF, WEBP, or MP4 Max 30mb
+                </p>
+              </>
+            )}
+            {fileName && (
+              <label htmlFor="file" className="mb">
+                <strong>File Uploaded: </strong>
+                {fileName}
+              </label>
+            )}
+
+            <Form className="mt-5">
+              <Form.Group as={Row} controlId="formPlaintextPassword">
+                <Form.Label column sm="3">
+                  NFT Name
+                </Form.Label>
+                <Col sm="9" className="align-self-center">
+                  <Form.Control type="text" placeholder="Eg. Blue ZUT Statue" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="formPlaintextPassword">
+                <Form.Label column sm="3">
+                  Symbol
+                </Form.Label>
+                <Col sm="9" className="align-self-center">
+                  <Form.Control type="text" placeholder="ZUT, ETH, ZRX ..." />
+                </Col>
+              </Form.Group>
+            </Form>
+            <p className="mt-5">
+              Please add specialized properties to the NFT (select one or
+              multiple):{" "}
+            </p>
+          </div>
+
+          {/* Option 1: Min Balance in Owner Wallet */}
+          <div className="ml-3">
+            <Form.Check type={"checkbox"}>
+              <Form.Check.Input
+                type={"checkbox"}
+                onChange={(e) => setOption1Checked(e.target.checked)}
+              />
               <Form.Check.Label>{`Holder must hold specific quantity of tokens`}</Form.Check.Label>
             </Form.Check>
+
+            {option1Checked && (
+              <Form className="mt-2 ml-2">
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                  <Form.Label column sm="3">
+                    Token Address
+                  </Form.Label>
+                  <Col sm="9" className="align-self-center">
+                    <Form.Control
+                      type="text"
+                      placeholder="Eg. 0x514910771af9ca656af840dff83e8264ecf986ca"
+                      onChange={(e) => setTokenAddress(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                  <Form.Label column sm="3">
+                    Minimum Balance
+                  </Form.Label>
+                  <Col sm="9" className="align-self-center">
+                    <Form.Control
+                      type="text"
+                      placeholder="Eg. 1000"
+                      onChange={(e) => setMinBalance(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+              </Form>
+            )}
           </div>
-          <div className="mt-4">
-            <Form.Check className="ml-3" type={"checkbox"}>
-              <Form.Check.Input type={"checkbox"} />
+
+          {/* Option 2: Expiration Time */}
+          <div className="mt-4 ml-3">
+            <Form.Check type={"checkbox"}>
+              <Form.Check.Input
+                type={"checkbox"}
+                onChange={(e) => setOption2Checked(e.target.checked)}
+              />
               <Form.Check.Label>{`NFT Auto Destructs on a timer`}</Form.Check.Label>
             </Form.Check>
+            {option2Checked && (
+              <Form className="mt-2 ml-2">
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                  <Form.Label column sm="3">
+                    Expiration Time
+                  </Form.Label>
+                  <Col sm="9" className="align-self-center">
+                    <Form.Control
+                      type="number"
+                      placeholder="Eg. 1608497905"
+                      onChange={(e) => setExpirationTime(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+              </Form>
+            )}
           </div>
+
+          {/* Token Creation */}
           <div className="mt-4">
-            <Image src={forgeButton} className="forge-button" />
+            <Image
+              src={forgeButton}
+              className="forge-button"
+              onClick={createToken}
+            />
           </div>
         </Col>
       </Row>
