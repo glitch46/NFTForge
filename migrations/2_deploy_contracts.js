3c2ede1ab60943ffa35abf53d0597fd38dddb744
wafflemakr <wafflemakr@gmail.com>
2020-12-23 10:47:50 -0500

    migrations with upgradeable contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
 const { deployProxy, upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 const ForgeToken = artifacts.require("ForgeToken");
-const ZutToken = artifacts.require("ZUTMock");
-const Triggerer = artifacts.require("Triggerer");
+const ZutToken = artifacts.require("ZUT");
 
 const ETH_FEE = web3.utils.toWei("0.02");
 const ZUT_FEE = web3.utils.toWei("0.03");
@@ -12,21 +11,19 @@ const FEE_RECIPIENT = "0xD90981ee801ea9ccFDC4AEFFbD14a81989463c95";
 module.exports = async function (deployer, network, accounts) {
   if (network === "development") return;
 
-  await deployer.deploy(ZutToken);
-  await deployer.deploy(
-    ForgeToken,
-    ZutToken.address,
-    FEE_RECIPIENT,
-    ETH_FEE,
-    ZUT_FEE
-  );
+  // DEPLOY PROXY ZUT
+  const zutToken = await deployProxy(ZutToken, ["Zero Utility Token", "ZUT"], {
+    deployer,
+  });
 
-  // DEPLOY PROXY
-  const instance = await deployProxy(
-    Triggerer,
-    [ForgeToken.address, ZutToken.address, FEE_RECIPIENT, ETH_FEE, ZUT_FEE],
+  console.log("Deployed ZUT", zutToken.address);
+
+  // DEPLOY PROXY FORGE
+  const forgeToken = await deployProxy(
+    ForgeToken,
+    [zutToken.address, FEE_RECIPIENT, ETH_FEE, ZUT_FEE],
     { deployer }
   );
 
-  console.log("Deployed", instance.address);
+  console.log("Deployed Forge", forgeToken.address);
 };
